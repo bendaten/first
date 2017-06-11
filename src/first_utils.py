1f13a146440e19104746868efeb9b5f71fce518d
Daniel BenDavid <daniel.bendavid@adap.tv>
2017-06-10 19:32:08 -0700

    Added docstrings to FirstUtils

--- src/first_utils.py
+++ src/first_utils.py
@@ -5,6 +5,13 @@ class FirstUtils(object):
 
     @staticmethod
     def is_internet_on():
+
+        """
+        Check if connected to the internet to enable email validation
+
+        :return:
+        :rtype: bool
+        """
         try:
             urllib2.urlopen('http://216.58.192.142', timeout=1)
             return True
